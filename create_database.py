4b6c5a39aad4a6222314dab82aa49ffb230b11e7
Kacper <kacper@kacper-debian.kacper-debian>
2019-01-11 21:49:28 +0100

    Dodaje modul create_database, ktory zapisuje w pliku pickle slownik przechowujacy dane temperaturowe posortowane po dacie dd/mm oraz modul dla testow jednostkowych

--- create_database.py
+++ create_database.py
@@ -1,14 +1,18 @@
-"""Ten modul tworzy slownik zawierajacy dane pogodowe pogrupowane po dacie dd/mm"""
+"""Ten modul tworzy slownik DATA zawierajacy dane pogodowe pogrupowane po dacie dd/mm"""
 
 import load_data
-
+import pickle
+from collections import namedtuple
 
 station_id = 424
 years = (2001, 2002, 2003)
-
 DATA = {}
+DateEntry = namedtuple('DateEntry',['year','temperature'])
 
-def collect_temperatures_by_year(years, station_id):
+def collect_temperatures(years, station_id):
+	'''dodaje do slownika DATA kolejne rekordy
+	kluczem jest data w formacie dd/mm
+	wartoscia jest lista named tupli, ktore zawieraja informacje o roku'''
 	for year in years:
 		temperature_data = load_data.TemperatureData(year, station_id)
 		rows = temperature_data.read()
@@ -16,10 +20,18 @@ def collect_temperatures_by_year(years, station_id):
 			date_ddmm = "{0}/{1}".format(row[4],row[3])
 			year = row[2]
 			temperature = row[7]
-			#try:
-				#dodac kod, ktory bedzie dopisywal wartosci 
-			#except KeyError:
-				#lub tworzyl nowy wpis jesli jeszcze nie ma
-collect_temperatures_by_year(years,station_id)
-		
-		
+			row_entry = DateEntry(year, temperature)
+			try:
+				DATA[date_ddmm].append(row_entry) 
+			except KeyError:
+				DATA[date_ddmm] = [row_entry]
+
+def save_dict_to_file():
+	'''zapisuje slownik DATA do pliku data.pickle'''
+	with open('data.pickle', 'wb') as f:
+		pickle.dump(DATA, f)
+		f.close()
+				
+if __name__ == '__main__':
+	collect_temperatures(years,station_id)
+	save_dict_to_file()
