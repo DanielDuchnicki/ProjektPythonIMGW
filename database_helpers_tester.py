8aa76b798fe119524d7ddd6fa142e89a548259ab
Kacper <kacper@kacper-debian.kacper-debian>
2019-01-12 19:36:04 +0100

    commit wlasciwego pliku z testami

--- database_helpers_tester.py
+++ database_helpers_tester.py
@@ -1,20 +1,46 @@
 import unittest
+import pickle
+from collections import namedtuple
 
-import create_database
+import database_helpers
 
 
-class TestCreateDatabaseModule(unittest.TestCase):
+DateEntry = namedtuple('DateEntry',['year','temperature'])
+
+
+class TestDatabaseCreator(unittest.TestCase):
 	
-	def test_create_database(self):
-		"""Test dla tworzenia bazy danych.
-		Oczekujemy, ze znajdziemy '-1.1' pod kluczem '2001' dla 01/02"""
-		station_id = 424
-		years = (2001, 2002, 2003)
-		raw_temperature_data = load_data.TemperatureData(year, station_id)
-		rows = raw_temperature_data.read()
-		db = create_database.new(years, station_id)
-		Self.assertIn('-1.1', db.get_exact_year_temperature("01/02",2001)
+	#def test_create_database(self):
+		#TODO: poprawic helpers lub test tak, zeby test dzialal
+		#'''Test dla tworzenia bazy danych.'''
+		#station_id = 424
+		#years = (2001, 2002, 2003)
+		#test_rows = [[0,0,2001,02,01,0,0,-5],[0,0,2002,06,04,0,0,20.5],[0,0,2003,01,01,0,0,3]]
+		#db = DatabaseCreator(years, station_id) #tutaj powinien byc uzyty mock test_rows
+		#Self.assertIn('-1.1', db.get_exact_year_temperature("01/02",2001)
 
+	def test_save_to_file(self):
+		db = database_helpers.DatabaseCreator([2001],424)
+		test_entry =  DateEntry(2001, -2.5)
+		test_entry2 = DateEntry(2002, 2)
+		db.data = {"01/01":[test_entry, test_entry2]}
+		db.save_to_file('db_test.pickle')
+		with open('db_test.pickle', 'rb') as f:
+			pickle_data = pickle.load(f)
+		self.assertEqual(test_entry, pickle_data["01/01"][0])
+		self.assertEqual(test_entry2, pickle_data["01/01"][1])
+
+class TestDatabaseReader(unittest.TestCase):
+
+	def test_load_data(self):
+		some_data = {"foo":1, "bar":"fgsfds"}
+		with open('db_test.pickle', 'wb') as f:
+			pickle.dump(some_data, f)
+			f.close
+		loaded_data = database_helpers.DatabaseReader('db_test.pickle').data
+		self.assertEqual(loaded_data['foo'], 1)
+		self.assertEqual(loaded_data['bar'], "fgsfds")
+		
 if __name__ == '__main__':
 	unittest.main()
 
