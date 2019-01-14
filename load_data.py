a0dec85c14a1b731ed5e3d83387e266904082ac2
Kacper <kacper@kacper-debian.kacper-debian>
2019-01-15 00:13:43 +0100

    dopasowuje modul load_data do modulu download_files, dodaje test dla ladowania danych z lat 1966-1970

--- load_data.py
+++ load_data.py
@@ -1,6 +1,4 @@
-import zipfile
 import csv
-from io import StringIO
 
 
 class TemperatureData:
@@ -22,32 +20,30 @@ class TemperatureData:
 	def read(self):
 		'''metoda tworzaca liste wierszy, pobranych z pliku csv,
 		ktory znajduje sie w archiwum w katalogu data'''
-
-		data = StringIO(self.zip_file.read(self.file_name).decode('ISO-8859-1'))
-		rows = csv.reader(data)
+		rows = csv.reader(self.data)
 		return rows
+
 	
 class NewTemperatureData(TemperatureData):
 	"""klasa obslugujaca archiwa danych temperaturowych z lat 2001-2017
 	skladowe: rok, id stacji"""
 	
 	def __init__(self, year, station_id):
-		self.zip_path = "data/{0}_{1}_s.zip".format(self.year, self.station_id)
-		self.file_name = "s_d_{0}_{1}.csv".format(self.station_id, self.year)
-		self.zip_file = zipfile.ZipFile(self.zip_path)
+		self.file_name = "data/s_d_{0}_{1}.csv".format(self.station_id, self.year)
+		self.data = open(self.file_name, 'r', encoding='ISO-8859-1')
+		
 		
 class OldTemperatureData(TemperatureData):
 	"""klasa obslugujaca archiwa z lat 1966-2000
 	skladowe: pierwszy_rok, id stacji"""
 	
 	def __init__(self, year, station_id):
-		self.zip_path = "data/{0}_{1}_{2}_s.zip".format(self.year, self.year + 4, self.station_id)
-		self.file_name = "s_d_{0}_{1}_{2}.csv".format(self.station_id, self.year, self.year + 4)
-		self.zip_file = zipfile.ZipFile(self.zip_path)
+		self.file_name = "data/s_d_{0}_{1}_{2}.csv".format(self.station_id, self.year, self.year + 4)
+		self.data = open(self.file_name, 'r', encoding='ISO-8859-1')
+
 	
 class TemperatureData1960(TemperatureData):
 	"""Klasa dla pliku 1960-1965, ktory jest wyjatkiem i obsluguje 6 lat, a nie 5"""
 	def __init__(self, year, station_id):
-		self.zip_path = "data/{0}_{1}_{2}_s.zip".format(self.year, self.year + 5, self.station_id)
-		self.file_name = "s_d_{0}_{1}_{2}.csv".format(self.station_id, self.year, self.year + 5)
-		self.zip_file = zipfile.ZipFile(self.zip_path)
+		self.file_name = "data/s_d_{0}_{1}_{2}.csv".format(self.station_id, self.year, self.year + 5)
+		self.data = open(self.file_name, 'r', encoding='ISO-8859-1')
--- load_data_tester.py
+++ load_data_tester.py
@@ -15,6 +15,20 @@ class TestLoadData(unittest.TestCase):
 		for row in rows:
 			self.assertIn(str(year), row)
 			break
+	
+	def test_1966(self):
+		"""przypadek dla ladowania danych z lat 1966-1970"""
+		station_id = 424
+		year = 1966
+		temperature_data = load_data.TemperatureData(year, station_id)
+		rows = temperature_data.read()
+		years = []
+		for row in rows:
+			years.append(row[2])
+		self.assertIn(str(year), years)
+		self.assertIn("1968", years)
+		self.assertIn("1969", years)
+
 
 if __name__ == '__main__':
 	unittest.main()
