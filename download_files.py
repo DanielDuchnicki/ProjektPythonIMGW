b73a622eaf892ea7282b4555e30be05f7af74e22
Daniel Duchnicki <duchnicki.daniel@gmail.com>
2019-01-14 20:44:11 +0100

    Zmiany w ściąganiu danych. Dodanie ściągania tylko dla wybranej stacji.

--- download_files.py
+++ download_files.py
@@ -11,35 +11,42 @@ def create_directories_links_list(url, dom):
     return directories_links_list
 
 
-def create_files_links_list(directories_links_list):
-    files_links_list = {}
+def create_files_links_dictionary(directories_links_list):
+    files_links_dictionary = {}
     for link in directories_links_list:
         request = requests.get(link)
         dom = lxml.html.fromstring(request.content)
         for file_link in dom.xpath('//tr[td/img[@alt="[   ]"]]/td//a/@href'):
-            files_links_list[file_link] = link + file_link
-    return files_links_list
+            files_links_dictionary[file_link] = link + file_link
+    return files_links_dictionary
 
 
-def download_zip_files(files_links_list):
+def select_files_by_station_id(files_links_dictionary, station_id):
+    selected_files_links_dictionary = {}
+    dictionary_keys = files_links_dictionary.keys()
+    for key in dictionary_keys:
+        if str(station_id) == key[-9:-6]:
+            selected_files_links_dictionary[key] = files_links_dictionary[key]
+    return selected_files_links_dictionary
+
+
+def download_zip_files(selected_files_links_dictionary):
     request = None
-    for file_name in files_links_list:
+    for file_name in selected_files_links_dictionary:
         status_code = 404
         while status_code != 200:
-            request = requests.get(files_links_list[file_name])
+            request = requests.get(selected_files_links_dictionary[file_name])
             status_code = request.status_code
         zip_file = zipfile.ZipFile(BytesIO(request.content))
-        zip_file.extractall('data/' + file_name)
+        zip_file.extractall('data/')
 
 
-def download_files():
+def download_files(station_id):
     url = "https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/synop/"
     request = requests.get(url)
     dom = lxml.html.fromstring(request.content)
 
     directories_links_list = create_directories_links_list(url, dom)
-    files_links_list = create_files_links_list(directories_links_list)
-    download_zip_files(files_links_list)
-
-
-download_files()
+    files_links_dictionary = create_files_links_dictionary(directories_links_list)
+    selected_files_links_dictionary = select_files_by_station_id(files_links_dictionary, station_id)
+    download_zip_files(selected_files_links_dictionary)
