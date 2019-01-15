b0278fc4948ff35d5be08ed22d6f04a2badd6754
Joanna Semeniuk <semeniuk.asia@gmail.com>
2019-01-15 09:39:14 +0100

    Poprawki do funkcji translate_date

--- count_temperature.py
+++ count_temperature.py
@@ -22,7 +22,7 @@ def weather_forecast(database):
     tomorrow = translate_date(tomorrow_date)
     day_after_tomorrow_date = tomorrow_date + timedelta(days=1)
     day_after_tomorrow = translate_date(day_after_tomorrow_date)
-    return {tomorrow: count_mean_temperature(tomorrow, database), day_after_tomorrow: count_mean_temperature(day_after_tomorrow, database)}
+    return {'tomorrow': count_mean_temperature(tomorrow, database), 'day after tomorrow': count_mean_temperature(day_after_tomorrow, database)}
 
 
 def count_mean_temperature(given_date, database):
@@ -31,7 +31,7 @@ def count_mean_temperature(given_date, database):
     :param database: object of DatabaseReader class
     :return: mean_temperature
     """
-    temperatures = [temp for temp in database.get_temperatures(given_date).values()]
+    temperatures = [float(temp) for temp in database.get_temperatures(given_date).values()]
     mean_temperature = sum(temperatures)/len(temperatures)
     return mean_temperature
 
@@ -41,5 +41,16 @@ def translate_date(given_date):
     :param given_date: date in datetime format - datetime.date(yyyy, mm, dd)
     :return: day_month: date in string format - 'dd/mm'
     """
-    day_month = str(given_date.day)+'/'+str(given_date.month)
+    month = given_date.month
+    day = given_date.day
+    if month < 9:
+        month_str = '0' + str(month)
+    else:
+        month_str = str(month)
+    if day < 9:
+        day_str = '0' + str(day)
+    else:
+        day_str = str(day)
+    day_month = day_str + '/' + month_str
     return day_month
+
